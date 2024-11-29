a2e6f8bee3c6180c6116c9308d2ffac48fdca67c
rahul soni <sonirahul9321@gmail.com>
2024-11-30 00:44:05 +0530

    Gemini Setup,Better Error Handling and Complete Change of Chat Interface UI

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -6,7 +6,9 @@ import { NextResponse } from "next/server";
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-
+  if (!GEMINI_API_KEY) {
+    throw new Error("Something went wrong while fetching API Keys.")
+  }
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
