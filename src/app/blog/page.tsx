9d001c2d95509e3cbd28f154b148c996c9dccbbc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-12-23 13:08:40 +0530

    Blog List: date formatted

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
+
 import { getAllPosts } from "@/lib/posts"
+import { formatDate } from "@/lib/utils"
 
 export default async function BlogPage() {
   const posts = await getAllPosts()
@@ -17,7 +19,7 @@ export default async function BlogPage() {
               {post.title}
             </Link>
             <p className="text-gray-500">
-              {post.publishedAt} • {post.views} views
+              {formatDate(new Date(post.publishedAt))} • {post.views} views
             </p>
             <p>{post.summary}</p>
           </li>
