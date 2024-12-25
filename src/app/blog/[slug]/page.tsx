a338bf50684ec759d306fbdb85095d1252079af4
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-12-25 21:44:19 +0530

    Blog: prose styling simplified, multiple overrides removed

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -26,46 +26,33 @@ const generateId = (children: any) => {
 }
 
 const components = {
-  h1: (props: any) => (
-    <h1 className="mb-4 mt-8 text-4xl font-bold" {...props}>
-      {props.children}
-    </h1>
-  ),
+  h1: (props: any) => <h1 {...props}>{props.children}</h1>,
   h2: (props: any) => (
-    <h2
-      id={generateId(props.children)}
-      className="mb-3 mt-6 text-3xl font-semibold"
-      {...props}
-    >
+    <h2 id={generateId(props.children)} {...props}>
       {props.children}
     </h2>
   ),
   h3: (props: any) => (
-    <h3
-      id={generateId(props.children)}
-      className="mb-2 mt-4 text-2xl font-semibold"
-      {...props}
-    >
+    <h3 id={generateId(props.children)} {...props}>
       {props.children}
     </h3>
   ),
   h4: (props: any) => (
-    <h4
-      id={generateId(props.children)}
-      className="mb-2 mt-3 text-xl font-semibold"
-      {...props}
-    >
+    <h4 id={generateId(props.children)} {...props}>
       {props.children}
     </h4>
   ),
-  p: (props: any) => <p className="my-2" {...props} />,
+  p: (props: any) => <p className="opacity-80 my-2" {...props} />,
   a: (props: any) => (
-    <Link className="text-blue-500 hover:underline" {...props} />
+    <Link
+      className="text-blue-400 no-underline hover:underline hover:text-blue-500"
+      {...props}
+    />
   ),
   Image: (props: any) => <Image className="my-4" alt={props.alt} {...props} />,
   blockquote: (props: any) => (
     <blockquote
-      className="my-4 border-l-4 border-blue-500 text-neutral-300 pl-4 italic"
+      className="opacity-80 border-l-4 border-blue-500 pl-4"
       {...props}
     />
   ),
@@ -86,6 +73,7 @@ const components = {
   td: (props: any) => (
     <td className="border border-neutral-500 px-4 py-2" {...props} />
   ),
+  hr: (props: any) => <hr className="opacity-50 my-12" {...props} />,
 }
 
 export default async function BlogPost({
@@ -118,11 +106,9 @@ export default async function BlogPost({
         <div className="w-full lg:w-3/4">
           <article
             className={cn(
-              "prose prose-neutral prose-invert prose-lg max-w-none text-white",
-              "prose-blockquote:opacity-80 prose-hr:opacity-50",
-              "prose-p:opacity-80 prose-ul:opacity-80 prose-ol:opacity-80",
-              "prose-headings:font-semibold prose-headings:tracking-tight prose-headings:opacity-85",
-              "prose-h1:tracking-tight prose-h1:mt-24 prose-h1:mb-0 prose-h1:text-3xl prose-h1:leading-snug"
+              "prose prose-neutral prose-invert prose-lg max-w-none text-neutral-200",
+              "prose-ul:opacity-80 prose-ol:opacity-80",
+              "prose-headings:font-semibold prose-headings:tracking-tight prose-headings:opacity-85"
             )}
           >
             <MDXRemote
