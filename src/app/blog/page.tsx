5c8b3da44b7d8cadd633b5479f81c70e7a49fbc2
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-13 01:41:38 +0530

    Blogs: key prop issue fixed

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -13,30 +13,28 @@ export default async function BlogPage() {
       </h1>
       <div className="-mx-3 flex flex-col gap-3">
         {posts.map((post) => (
-          <>
-            <Link
-              href={`/blog/${post.slug}`}
-              key={post.slug}
-              className="flex items-start gap-6 rounded-lg p-4 transition-colors hover:bg-neutral-900 focus:bg-neutral-900"
-            >
-              <div className="w-3/12 flex-1">
-                <p className="mb-1 mt-1.5 text-xs text-neutral-500 md:text-sm">
-                  {formatDate(new Date(post.publishedAt))}
-                </p>
-                <p className="text-xs text-neutral-500 md:text-sm">
-                  {post.views} views
-                </p>
-              </div>
-              <div className="w-9/12">
-                <p className="mb-1 text-xl font-semibold tracking-tight text-neutral-300 md:text-2xl">
-                  {post.title}
-                </p>
-                <p className="mb-2.5 text-sm text-neutral-500 md:text-base">
-                  {post.summary}
-                </p>
-              </div>
-            </Link>
-          </>
+          <Link
+            href={`/blog/${post.slug}`}
+            key={post.slug}
+            className="flex items-start gap-6 rounded-lg p-4 transition-colors hover:bg-neutral-900 focus:bg-neutral-900"
+          >
+            <div className="w-3/12 flex-1">
+              <p className="mb-1 mt-1.5 text-xs text-neutral-500 md:text-sm">
+                {formatDate(new Date(post.publishedAt))}
+              </p>
+              <p className="text-xs text-neutral-500 md:text-sm">
+                {post.views} views
+              </p>
+            </div>
+            <div className="w-9/12">
+              <p className="mb-1 text-xl font-semibold tracking-tight text-neutral-300 md:text-2xl">
+                {post.title}
+              </p>
+              <p className="mb-2.5 text-sm text-neutral-500 md:text-base">
+                {post.summary}
+              </p>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
