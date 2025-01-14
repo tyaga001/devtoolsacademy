0da387f821d420b34998a915241b79cd938b4d54
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-14 23:44:19 +0530

    Not Found Page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+import * as React from "react"
+
+interface Props {}
+
+const NotFoundPage: React.FC<Props> = () => {
+  return (
+    <section className="grid min-h-screen place-content-center">
+      <div className="text-center">
+        <h1 className="mb-3 text-9xl font-semibold tracking-tighter text-neutral-300">
+          404
+        </h1>
+        <p className="text-base text-neutral-300">
+          Page not found, go{" "}
+          <Link
+            href="/"
+            className="font-semibold hover:underline focus:underline"
+          >
+            Home
+          </Link>
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default NotFoundPage
