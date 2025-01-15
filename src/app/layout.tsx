7350ff551546698653140d6647a082434f06af95
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-16 02:09:05 +0530

    Old metadata config removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,16 @@
-import type { Metadata, Viewport } from "next"
-import { ViewTransitions } from "next-view-transitions"
+import * as React from "react"
+import type { Viewport } from "next"
 import Script from "next/script"
 import { Inter, JetBrains_Mono } from "next/font/google"
-
-import * as React from "react"
+import { ViewTransitions } from "next-view-transitions"
 
 import { Analytics } from "@vercel/analytics/react"
 import { ClerkProvider } from "@clerk/nextjs"
 
 import Navbar from "@/components/Navbar"
-import { SocialMetadata } from "@/components/SocialMetadata"
 import { cn } from "@/lib/utils"
 
 import "./globals.css"
-
-export const metadata: Metadata = {
-  title: {
-    default: "Dev Tools Academy",
-    template: "%s | Dev Tools Academy",
-  },
-  description: "Learn about awesome developer tools",
-  metadataBase: new URL("https://devtoolsacademy.com"),
-  openGraph: {
-    title: "Dev Tools Academy",
-    description: "Learn about awesome developer tools",
-    url: "https://devtoolsacademy.com",
-    siteName: "Dev Tools Academy",
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Dev Tools Academy",
-    description: "Learn about awesome developer tools",
-  },
-  icons: [
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      url: "/favicon.png",
-    },
-  ],
-}
 
 const sansFont = Inter({
   variable: "--sans-font",
@@ -62,31 +30,11 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
-function getTitle(title: Metadata["title"]): string {
-  if (typeof title === "string") {
-    return title
-  } else if (title && typeof title === "object" && "default" in title) {
-    return title.default
-  }
-  return "Dev Tools Academy"
-}
-
 export default function RootLayout({ children }: RootLayoutProps) {
-  const title = getTitle(metadata.title)
-
   return (
     <ViewTransitions>
       <html lang="en">
         <head>
-          <SocialMetadata
-            title={title}
-            description={
-              metadata.description ?? "Learn about awesome developer tools"
-            }
-            path={"/"}
-            image={`${metadata.metadataBase?.toString() ?? "https://devtoolsacademy.com"}/favicon.png`}
-            type="website"
-          />
           <Script
             src="https://cloud.umami.is/script.js"
             data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
