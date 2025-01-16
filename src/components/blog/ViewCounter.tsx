5215fecf34c3cae3f8a3f9bffbbb95aa8fb6ef88
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-16 15:05:38 +0530

    View Counter: fetch data from API

--- src/components/blog/ViewCounter.tsx
+++ src/components/blog/ViewCounter.tsx
@@ -1,42 +1,36 @@
 "use client"
 
 import * as React from "react"
+
 import { Eye } from "lucide-react"
 
-export default function ViewCounter({
-  slug,
-  initialViews,
-}: {
-  slug: string
-  initialViews: number
-}) {
-  const [views, setViews] = React.useState(initialViews)
+export default function ViewCounter() {
+  const [views, setViews] = React.useState(0)
 
   React.useEffect(() => {
-    const incrementViews = async () => {
-      try {
-        const response = await fetch(`/api/views/`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ slug: slug }),
-        })
-        const data = await response.json()
-        setViews(data.views)
-      } catch (error) {
-        console.error("Failed to increment view count:", error)
+    if (window) {
+      const slug = window.location.pathname
+        .replace("blog", "")
+        .replaceAll("/", "")
+
+      const fetchViews = async () => {
+        try {
+          const response = await fetch(`/api/views/?slug=${slug}`)
+          const data = await response.json()
+          console.log(data)
+          // setViews(data.views)
+        } catch (error) {
+          console.error("Failed to fetch view count:", error)
+        }
       }
+      fetchViews()
     }
-    incrementViews()
-  }, [slug])
+  }, [])
 
   return (
     <div className="group flex items-center rounded-full bg-neutral-900 px-3 py-1.5 text-neutral-400">
       <Eye className="mr-2 size-5 group-hover:text-blue-400" />
-      <span className="text-sm font-medium">
-        {views.toLocaleString()} views
-      </span>
+      <span className="text-sm font-medium">{views} views</span>
     </div>
   )
 }
