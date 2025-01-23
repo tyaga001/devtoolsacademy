d3975528049dc0445e34a2eb2e789c46e481e1bd
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-23 20:28:35 +0530

    remote patterns added for external images

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh7-rt.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "tender-bugle-dd6.notion.site",
+      },
+    ],
+  },
+
   experimental: {
     mdxRs: true,
   },
