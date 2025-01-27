3408e5620e3f05660c12807f51121ca30bc74b57
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-28 00:11:05 +0530

    Algolia Fixes

--- src/components/tools/AlgoliaSearch.tsx
+++ src/components/tools/AlgoliaSearch.tsx
@@ -18,7 +18,7 @@ export const getTools = async (text: string): Promise<ToolCardInterface[]> => {
     process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY!
   )
 
-  const index = client.initIndex("data")
+  const index = client.initIndex("toolData")
   const { hits } = await index.search<ToolCardInterface>(text, {
     hitsPerPage: 20,
   })
