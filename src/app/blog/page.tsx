5bab8e9580300ba062e9784abe5abe1b455df6bd
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-02-24 03:01:41 +0530

    Blog List: Redesign

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,6 @@
 import { Link } from "next-view-transitions"
 
-import { formatDate } from "@/lib/utils"
+import { cn, formatDate } from "@/lib/utils"
 import { getMetadata } from "@/lib/metadata"
 
 import { allBlogs } from "./data"
@@ -13,30 +13,42 @@ export const metadata = getMetadata({
 
 export default async function BlogPage() {
   return (
-    <div className="mx-auto max-w-4xl px-4 py-36">
-      <h1 className="mb-8 text-3xl font-bold tracking-tight text-neutral-200 md:mb-20 md:text-5xl">
-        Blog Posts
-      </h1>
-      <div className="-mx-3 flex flex-col">
-        {allBlogs.map((post) => (
-          <Link
-            href={`/blog/${post.slug}`}
-            key={post.slug}
-            className="rounded-lg p-4 transition-colors hover:bg-neutral-900 focus:bg-neutral-900"
-          >
-            <p className="mb-1 text-xl font-semibold tracking-tight text-neutral-300 md:text-2xl">
-              {post.title}
-            </p>
-            <p className="mb-2.5 text-sm text-neutral-500 md:text-base">
-              {post.description}
-            </p>
-            <p className="flex items-center gap-2 text-xs text-neutral-500 md:text-sm">
-              <span>{post.author}</span>
-              <span>·</span>
-              <span>{formatDate(new Date(post.publishedAt))}</span>
-            </p>
-          </Link>
-        ))}
+    <div className="py-36">
+      <div className="text-center">
+        <h1 className="mb-4 text-2xl font-bold tracking-tight md:text-5xl">
+          <span className="bg-gradient-to-b from-neutral-700 to-neutral-200 bg-clip-text text-transparent">
+            Blog Posts
+          </span>
+        </h1>
+      </div>
+      <div className="mx-auto max-w-7xl py-16">
+        <div className="flex flex-col">
+          <hr className="border-dashed border-[#f6f6f6] opacity-10" />
+          {allBlogs.map((post) => (
+            <Link
+              key={post.slug}
+              href={`/blog/${post.slug}`}
+              className={cn(
+                "p-4 transition-colors hover:bg-neutral-900 focus:bg-neutral-900",
+                "border-b border-dashed border-[#f6f6f6]/10",
+                "flex"
+              )}
+            >
+              <div className="flex w-2/12 flex-col text-xs text-neutral-500 md:text-sm">
+                <p className="mb-3">{formatDate(new Date(post.publishedAt))}</p>
+                <p>by {post.author}</p>
+              </div>
+              <div className="w-10/12">
+                <p className="mb-3 text-xl font-semibold tracking-tight text-neutral-300 md:text-2xl">
+                  {post.title}
+                </p>
+                <p className="text-sm text-neutral-500 md:text-base">
+                  {post.description}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   )
