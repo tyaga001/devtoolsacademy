635af383f10ca5583c7ed56f1c51210b83c0d4fe
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-03-17 12:28:41 +0530

    Sitemap (#103)
    * sitemap added
    
    * static sitemap removed
    
    * metadata: base url fix
    
    * dynamic sitemap added
    
    * sitemap: import order
    
    * sitemap: tools links added

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
 
-export const baseUrl = "https://www.devtoolsacademy.com/"
+export const baseUrl = "https://www.devtoolsacademy.com"
 
 const config = {
   baseUrl,
