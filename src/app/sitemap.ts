d150a30dc0de856bf91ebfa5b9039363e26f14e6
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-05-08 16:44:41 +0530

    Sitemap: force dynamic (#118)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,6 +4,8 @@ import { allBlogs } from "@/app/blog/data"
 import { baseUrl } from "@/lib/metadata"
 import prisma from "@/lib/prisma"
 
+export const dynamic = "force-dynamic"
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blogPosts = allBlogs.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}/`,
